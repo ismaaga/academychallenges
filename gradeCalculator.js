e0ec91f79309469ad8c61804cdf786a41d8018c4
ismaaga <70133028+ismaaga@users.noreply.github.com>
2023-10-31 09:26:55 +0100

    Add files via upload
    Grade Cal

---
+++ gradeCalculator.js
@@ -0,0 +1,14 @@
+var score= prompt('What is your score');
+if (score >=70){
+    alert( 'You score an A');
+}else if (score>=60)
+{
+    alert('You score a C');
+} else if (score>=50)
+{
+    alert('You score a C');
+}
+else{ 
+    alert ('You score an F');
+}
+    
